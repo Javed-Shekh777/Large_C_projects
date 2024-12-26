06628e96ef67c46106f71cb6ec2c051533996cc8
Javed-Shekh777 <mdjavedshekh777>
2024-12-26 11:25:33 +0530

    last

--- numberGuessingGame/numberGuessing.c
+++ numberGuessingGame/numberGuessing.c
@@ -1,37 +1,30 @@
-#include <stdio.h>
-#include <time.h>
-#include<stdlib.h>
-#define NUMBER 100
-
-void main()
-{
-    int guessNumber, randomNumber, noOfAttemps = 0;
-
-    srand(time(NULL));
-    printf("-------------------- Welcome To You In Number Guessing Game -----------------------------------\n\n");
-    randomNumber = rand() % 100 +1;
-
-    do
-    {
-        printf("\nPlease enter a number between (1 to %d) : ", NUMBER);
-        scanf("%d", &guessNumber);
-        noOfAttemps++;
-        if (guessNumber < randomNumber)
-        {
-            printf("\nGuess a Greter number \n");
-        }
-        else if (guessNumber > randomNumber)
-        {
-            printf("\nGuess a Lower number ");
-        }
-        else
-        {
-            printf("\n\n!!Congratulation , You have guessed successfully number. \n in %d attempts.", noOfAttemps);
-        }
-         
-
-    } while (guessNumber != randomNumber);
-
-    printf("\n\n\t\tThanks for playing the game.....");
-    printf("\n\n\t\t\tDeveloped by : Javed Akhtar");
+#include<stdio.h>
+void main(){
+	int n,temp,len=0,num=0;
+	printf("Enter a number : ");
+	scanf("%d",&n);
+	temp=n;
+	while(n>0){
+		n/=10;
+		len++;
+	}
+	n=temp;
+	while(n>0){
+		int rem = n%10;
+		printf("1 : %d\nNN : %d",rem);
+		for(int i=0;i<len;i++){
+			rem = rem*rem;
+		}
+		
+	 
+		printf("Rem : %d\nNum : %d\n",rem);
+		n/=10;
+	}
+	printf("%d\n%d",num,temp);
+	if(num==temp){
+		printf("Number is Armstrong");
+	}else{
+			printf("Number is not Armstrong");
+	}
+	
 }
